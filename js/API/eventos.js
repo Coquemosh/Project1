92a9d2aae34e7b0e5c5cd5a14f1f34b1f434d91d
Jorge Lira <coque_16@hotmail.com>
2013-06-22 18:24:55 -0500

    Change eventos

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -1,57 +1,26 @@
-// Eventos
-/*$("document").ready(function(e) {
-  alert("s");  
-});; *//* jQuery() o $() es lo mismo
-                * $("document") o $(document) es lo mismo*/
-
-// $(function(e) donde la letra "e" es el evento que nos regresa la función
-/*$(function(e) {
-  //alert("s");
-  // $('#login ul li:eq(1)').hide();
- //$('#regNom').hide(); // hide para ocultar el elemento, en este caso el nombre
-});*/
-//$(function(e) {
-$(document).ready(function(e) {
-  document.addEventListener("deviceready",function(){
-  //$('#login ul li:eq(5)').hide(); // "eq" se refiere el elemento número 5
-  
- /* $('#regSend').click(function(){
-	alert("s");  
-  });*/
-  
-  if(!isLogin()){
-	  window.location.href = '#login';
-  }
-  
-   $('#regSend').tap(function(){ // tap 1
-	if($('#regNom').val() == '' || $('#regTel').val()== '' || 
-	   $('#regMail').val()== '' || $('#regFoto').attr('rel')==undefined ){
-		navigator.notification.alert('Todos los campos son requeridos',null,"Hotel", "Ok");
-	}else{
-		var nom  = $('#regNom').val();
-		var tel  = $('#regTel').val();
-		var mail = $('#regMail').val();
-		var foto = $('#regFoto').attr('rel');
-		
-		//navigator.notification.alert(nom + '\n' + tel + '\n' + mail,null, "Hotel", "Aceptar");
-		enviarDatos(nom,tel,mail.foto);
-	}
-  }); // tap 1
-  $('#regFoto').tap(function(){ // tap 2
-	  tomarFoto();
-	}); // end tap 2
- }, false); // End Event Listener
-  /* "click" a diferencia de "tab" es que el tab es como si dieras el click antes de soltar el click.*/
-}); // end Document Ready
-/*
-function loaddata(){
-	
-}*/
+//Eventos
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		if(!isLogin())
+			window.location.href = '#login';
+	   $('#regSend').tap(function(){
+		  if($('#regNom').val()!='' && $('#regTel').val()!='' && $('#regMail').val()!='' && $('#regFoto').attr('rel')!=undefined){
+			  var nom = $('#regNom').val();
+			  var tel = $('#regTel').val();
+			  var mail = $('#regMail').val();
+			  var foto = $('#regFoto').attr('rel');
+			  enviarDatos(nom, tel, mail, foto);
+			  //navigator.notification.alert(nom +'\n'+ tel +'\n'+ mail, null, "Hotel","Aceptar");
+		  }else{
+			  navigator.notification.alert('Todos los campos son requeridos', null, "Hotel","Ok");
+		  }
+	   });
+	   $('#regFoto').tap(function(){
+		   tomarFoto();
+	   });
+	}, false);
+});
 
 function isLogin(){
-	return false;	
-<<<<<<< HEAD
+	return false;
 }
-=======
-}
->>>>>>> be61f6b9c090f684432f2f5a7e594f15d84126d8
