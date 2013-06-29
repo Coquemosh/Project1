aac9edf1010f0c340d094dfb8909d78917fab09e
Jorge Lira <coque_16@hotmail.com>
2013-06-29 15:52:19 -0500

    Change icon

---
+++ js/API/dispositivo.js
@@ -0,0 +1,20 @@
+// Dispositivo
+
+function infoDisp(){
+	
+	var arr = [];
+	
+	arr[nombre]   = devide.name; //window.device.name;
+	arr[phonegab] = devide.cordova;
+	arr[platform] = devide.platform;
+	arr[uuid]     = devide.uuid;
+	arr[version]  = devide.version;
+	arr[model]    = devide.model;
+	
+	return arr;
+	/*
+	 * var arr = [];
+	 * var arr = new Array(); 
+	 * 3 formas de declarar arreglos en JS
+	*/
+}
