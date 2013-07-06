da6c4514f569f2c02f309ba6301b136736a37a14
Jorge Lira <coque_16@hotmail.com>
2013-07-06 17:24:10 -0500

    xc

--- js/API/conexion.js
+++ js/API/conexion.js
@@ -4,8 +4,12 @@ function isConnected(){
 	
 	// http://docs.phonegap.com/en/2.8.0/cordova_connection_connection.md.html#Connection
 	alert('isConnected');
-	if(navigation.connection.type!=Connection.NONE)
+	if(navigation.connection.type!=Connection.NONE){
+		alert('true');
 		return true;
-	else
+	}
+	else{
+		alert('false');
 		return false;
+	}
 }
